4edfd3faa70e1d6f098b01209134c4cf5dd95c18
robertjezyk <rjezyk@vp.pl>
2024-02-15 19:24:11 +0000

    storing all events in event bus for query service down backup

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,11 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+  events.push(event);
   axios
     .post("http://localhost:4000/events", event)
     .catch((error) => console.log(error.message));
@@ -21,6 +24,10 @@ app.post("/events", (req, res) => {
     .catch((error) => console.log(error.message));
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
