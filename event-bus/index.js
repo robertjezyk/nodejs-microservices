20b35f646b486dfdde2bbf5ed6aaaa2e94e7cc9b
robertjezyk <rjezyk@vp.pl>
2024-02-27 12:14:54 +0000

    wiring up posts and event-bus kubernetes services, so they communicate

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,17 +11,17 @@ app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event);
   axios
-    .post("http://localhost:4000/events", event)
+    .post("http://posts-clusterip-srv:4000/events", event)
     .catch((error) => console.log(error.message));
-  axios
-    .post("http://localhost:4001/events", event)
-    .catch((error) => console.log(error.message));
-  axios
-    .post("http://localhost:4002/events", event)
-    .catch((error) => console.log(error.message));
-  axios
-    .post("http://localhost:4003/events", event)
-    .catch((error) => console.log(error.message));
+  // axios
+  //   .post("http://localhost:4001/events", event)
+  //   .catch((error) => console.log(error.message));
+  // axios
+  //   .post("http://localhost:4002/events", event)
+  //   .catch((error) => console.log(error.message));
+  // axios
+  //   .post("http://localhost:4003/events", event)
+  //   .catch((error) => console.log(error.message));
 
   res.send({ status: "OK" });
 });
